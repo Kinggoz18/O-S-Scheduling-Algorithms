7ae9aea938ec7cd350931a425315597eb73508cb
Chigozie Muonagolu <77743335+Kinggoz18@users.noreply.github.com>
2023-02-01 14:31:47 -0500

    Initial Commit

---
+++ Lab2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab2
+//
+//  Created by Chigozie Muonagolu on 2023-02-01.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
