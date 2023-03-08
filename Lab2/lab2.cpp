03f39591a1273e2a72fcc7081e50d342c9d17e87
Chigozie Muonagolu <77743335+Kinggoz18@users.noreply.github.com>
2023-03-08 14:15:41 -0500

    Finished Software scheduling algorithm simulation

--- Lab2/lab2.cpp
+++ Lab2/lab2.cpp
@@ -6,8 +6,19 @@
 //
 
 #include <iostream>
+#include "Process.h"
+
+//Class to represent a process object
+class Process{
+public:
+    int ArrivalTime;
+    int JobLength;
+    int TimeLeft;
+    int TimeElapsed;
+};
 
 int main(int argc, const char * argv[]) {
+    Process processArray [5000];
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
